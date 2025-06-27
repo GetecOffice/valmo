b3ab48cd421246b66f8afc048bd1daf2fa04036b
GetecOffice <getec.plc@gmail.com>
2025-06-27 11:56:41 -0600

    settings

--- Valmo/settings.py
+++ Valmo/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Valmo.urls'
 
-CSRF_TRUSTED_ORIGINS = ['https://valmo.185b.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://valmo.up.railway.app']
 
 TEMPLATES = [
     {
