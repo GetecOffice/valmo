e6ede77bf8a398903cc43af631b13f2a8a39e90b
GetecOffice <getec.plc@gmail.com>
2025-07-03 01:58:58 -0600

    base de datos

--- Valmo/settings.py
+++ Valmo/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
